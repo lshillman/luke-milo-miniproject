9cf7c3bccba356cf013c716d60dee4c45bfeca34
lshillman <lshillman@users.noreply.github.com>
2022-07-05 14:42:05 -0700

    build query and send to the next page

--- assets/js/results.js
+++ assets/js/results.js
@@ -11,12 +11,6 @@ var userTypeSelection = $('#userFormatSelection');
 function executeSearch(e) {
     e.preventDefault();
     console.log("I got clicked! Doin a search now " + userQuery.val() + ", " + userTypeSelection.val());
-    if (userTypeSelection.val() != "choose-a-format") {
-        location.replace("./search-results.html?q=" + userQuery.val() + "&format=" + userTypeSelection.val());
-    } else {
-        location.replace("./search-results.html?q=" + userQuery.val() + "&format=");
-    }
-
 }
 
 searchformEl.submit(executeSearch);
